f5139b28818bdb6e00fd100a963c380001960838
Arnaud-Nova <calmelsarnaud@gmail.com>
2018-12-01 12:05:19 +0100

    Affichage du playground initial

--- game.js
+++ game.js
@@ -1,20 +1,3 @@
-var game = {
-    init: function(){
-       
-    },
-    cellWidth: 50,
-    tableau: document.querySelector('.terrain_de_jeu'),
-    types: {
-        'x': 'classic',
-        '*': 'wall',
-        'o': 'burger',
-        '-': 'goal'
-    },
-};
-
-
-document.addEventListener('DOMContentLoaded', game.init);
-
 var model = [
     'xxxxxxxxx**xx',
     'x********xx-x',
@@ -27,3 +10,53 @@ var model = [
     'x***xxxxxx*xx',
     'xxxxxx*****xx',
 ];
+
+
+var game = {
+    init: function(){
+    game.createPlayground();
+
+    
+    
+    
+    },
+    cellWidth: 50,
+    tableau: document.querySelector('.terrain_de_jeu'),
+    types: {
+        'x': 'classic',
+        '*': 'wall',
+        'o': 'burger',
+        '-': 'goal'
+    },
+
+    // générer le terrain de jeu
+    createPlayground: function() {
+        var width = model[0].length;
+        console.log(game.cellWidth);
+        var playgroundWidth = width * game.cellWidth;
+        game.tableau.style.width = playgroundWidth + 'px';
+        
+        for (var line = 0; line < model.length; line++ ) {
+            for (var column = 0; column < model[0].length; column++) {
+                var cell = document.createElement('div');
+                for (var type in game.types) {
+                    if (type == model[line][column]) {
+                        cell.classList = game.types[type] + ' square';
+                    }
+                }
+                cell.style.display = 'inline-block';
+                game.tableau.appendChild(cell);
+    
+            }
+        }
+    },
+};
+
+
+document.addEventListener('DOMContentLoaded', game.init);
+
+
+//détecter les appuies sur les touches du clavier
+
+    // déplacer le burger selon les appuis sur les touches
+
